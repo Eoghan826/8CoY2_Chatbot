2a72e7ffb5c71041b74f3f7dd6383b069369ab1c
Eoghan826 <Elau@connect.kellettschool.com>
2024-11-08 05:43:39 +0000

    Fri, Nov 8, 2024, 5:43 AM +00:00

---
+++ dice.py
@@ -0,0 +1,13 @@
+# dice rolling (dice.py)
+from random import randint
+
+def roll():
+    return randint(1,6)
+
+def rollNSidedDice(sides=6):
+    return randint(1,sides)
+
+def rollNDice(num, sides=6):
+    total = 0
+    for _ in range()num:
+        total+=
