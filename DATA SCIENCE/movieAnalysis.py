9e7c79744f44a0a9f6956ca935188e5eb8d493d4
Eoghan826 <Elau@connect.kellettschool.com>
2024-11-21 01:14:56 +0000

    Thu, Nov 21, 2024, 1:14 AM +00:00

--- DATA SCIENCE/movieAnalysis.py
+++ DATA SCIENCE/movieAnalysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
-datafile = pd.read_csv("Top Rated 9000 Movies TMDB.csv ")
+datafile = pd.read_csv("Top Rated 9000 Movies TMDB.csv")
 
-for i, record in dataFile.iterrows():
+for i, record in datafile.iterrows():
     if  "m" in record['title'] or "n" in record['title']:
         print(f"Title is {record['title']}")
