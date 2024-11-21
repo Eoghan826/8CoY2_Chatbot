7009905dd2d37d56e88cbd374ea659a646c7695a
Eoghan826 <Elau@connect.kellettschool.com>
2024-11-21 01:25:28 +0000

    Thu, Nov 21, 2024, 1:25 AM +00:00

--- DATA SCIENCE/movieAnalysis.py
+++ DATA SCIENCE/movieAnalysis.py
@@ -1,1 +1,6 @@
- 
+import pandas as pd
+datafile = pd.read_csv("Top Rated 9000 Movies TMDB.csv")
+
+for i, record in datafile.iterrows():
+    if  "Sigma" in record['a'] in record['title']:
+        print(f"Title is {record['title']}")
