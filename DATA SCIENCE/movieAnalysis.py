4da3c53c561fa788f37b50885d5a9b39cc0f766f
Eoghan826 <Elau@connect.kellettschool.com>
2024-11-21 01:39:26 +0000

    Thu, Nov 21, 2024, 1:39 AM +00:00

--- DATA SCIENCE/movieAnalysis.py
+++ DATA SCIENCE/movieAnalysis.py
@@ -2,5 +2,5 @@ import pandas as pd
 datafile = pd.read_csv("Top Rated 9000 Movies TMDB.csv")
 
 for i, record in datafile.iterrows():
-    if  "A" in record['title'] or "A" in record['title']:
-        print(f"Title is {record['title'][A]}")
+    if  "A" in record['title'][0] or "a" in record['title'][0]:
+        print(f"Title is {record['title']}")
