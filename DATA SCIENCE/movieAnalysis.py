88d53e5cb620c8b8afa3e8b60714278704d1bf3a
Eoghan826 <Elau@connect.kellettschool.com>
2024-11-27 03:53:07 +0000

    Wed, Nov 27, 2024, 3:53 AM +00:00

--- DATA SCIENCE/movieAnalysis.py
+++ DATA SCIENCE/movieAnalysis.py
@@ -2,7 +2,8 @@ import pandas as pd
 datafile = pd.read_csv("Top Rated 9000 Movies TMDB.csv")
 count_a = 0
 for i, record in datafile.iterrows():
-    if  not "a" in record['title'] or not "a" in record['title'] or not "E" in record['title'] or not "e" in record['title']or not "I" in record['title']or not "i" in record['title']or not "O" in record['title']or not "o" in record['title'] or not "U" in record['title'] or not "u" in record['title']:
+    if "a" in record['title'] or "a" in record['title'] or "E" in record['title'] or "e" in record['title']or "I" in record['title']or "i" in record['title']or "O" in record['title']or "o" in record['title'] or "U" in record['title'] or "u" in record['title']:
         count_a += 1
         print(f"Title is {record['title']}")
         print(count_a)
+        print(9000 - count_a)
