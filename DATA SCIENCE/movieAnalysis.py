9c9b7d03e270f610893f0e4181ebb144019ad27a
Eoghan826 <Elau@connect.kellettschool.com>
2024-11-27 03:24:09 +0000

    Wed, Nov 27, 2024, 3:24 AM +00:00

--- DATA SCIENCE/movieAnalysis.py
+++ DATA SCIENCE/movieAnalysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 datafile = pd.read_csv("Top Rated 9000 Movies TMDB.csv")
 count_a = 0
 for i, record in datafile.iterrows():
-    if  "m" in record['title'] or "M" in record['title']or "N" in record['title']or "n" in record['title']:
+    if  "Love" in record['title'] or "love" in record['title']:
         count_a += 1
         print(f"Title is {record['title']}")
         print(count_a)
