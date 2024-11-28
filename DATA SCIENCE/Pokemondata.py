fdcea2e182ec060463a2436f2e540efcd30bc276
Eoghan826 <Elau@connect.kellettschool.com>
2024-11-28 01:19:18 +0000

    Thu, Nov 28, 2024, 1:19 AM +00:00

--- DATA SCIENCE/Pokemondata.py
+++ DATA SCIENCE/Pokemondata.py
@@ -2,7 +2,7 @@ import pandas as pd
 datafile = pd.read_csv("Pokemon.csv")
 count_a = 0
 for i, record in datafile.iterrows():
-    if int"80" in record['HP'] or int"80" in record['HP']:
+    if 80 == int(record['HP']):
         count_a += 1
         print(f"This pokemons HP is {record['HP']}")
         print(count_a)
