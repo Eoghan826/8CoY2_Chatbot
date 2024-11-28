c25e7b3ee8dac1625248c9d0624f5b481a21fca5
Eoghan826 <Elau@connect.kellettschool.com>
2024-11-28 01:33:23 +0000

    Thu, Nov 28, 2024, 1:33 AM +00:00

--- DATA SCIENCE/Pokemondata.py
+++ DATA SCIENCE/Pokemondata.py
@@ -5,6 +5,6 @@ totala = 0
 for i, record in datafile.iterrows():
     if 80 < int(record['Sp. Atk ']):
         count_a += 1
-        totala += int(record['Legendary'])
+        totala += int(record['Sp. Atk'])
         print(count_a)
         print(totala)
