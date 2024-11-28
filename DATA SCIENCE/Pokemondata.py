984bc54a3fb31b4fd4dbc98e3ff609af22f5c0be
Eoghan826 <Elau@connect.kellettschool.com>
2024-11-28 01:42:21 +0000

    Thu, Nov 28, 2024, 1:42 AM +00:00

--- DATA SCIENCE/Pokemondata.py
+++ DATA SCIENCE/Pokemondata.py
@@ -1,9 +1,1 @@
-import pandas as pd
-datafile = pd.read_csv("Pokemon.csv")
-count_a = 0
-totala = 0
-for i, record in datafile.iterrows():
-    if 'Dragon' == (record['Type 2']):
-        count_a += 1
-        totala += str(record['Type 2'])
-        print(count_a)
+xc
