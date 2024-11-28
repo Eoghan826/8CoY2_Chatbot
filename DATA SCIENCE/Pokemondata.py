391995f90ab3c7d77030c8b1294725a6b2194859
Eoghan826 <Elau@connect.kellettschool.com>
2024-11-28 01:36:50 +0000

    Thu, Nov 28, 2024, 1:36 AM +00:00

--- DATA SCIENCE/Pokemondata.py
+++ DATA SCIENCE/Pokemondata.py
@@ -3,7 +3,7 @@ datafile = pd.read_csv("Pokemon.csv")
 count_a = 0
 totala = 0
 for i, record in datafile.iterrows():
-    if 80 < int(record['Sp. Atk ']):
+    if 80 < int(record['Sp. Atk']):
         count_a += 1
         totala += int(record['Sp. Atk'])
         print(count_a)
