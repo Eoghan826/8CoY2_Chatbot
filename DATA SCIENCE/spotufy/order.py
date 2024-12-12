d398c0964d22abcfe84ee9fd49bf238febcb6bbb
Eoghan826 <Elau@connect.kellettschool.com>
2024-12-12 03:57:39 +0000

    Thu, Dec 12, 2024, 3:57 AM +00:00

---
+++ DATA SCIENCE/spotufy/order.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import json
+datafile = pd.read_json("spotifyy.json")
+count_a = 0
+totala = 0
+RED = '\033[31m'
+RESET = '\033[0m'
+for i, record in datafile.iterrows():
+    trackName = record['master_metadata_track_name']
+    if trackName:
+        word = 'Beautiful'
+        if word in trackName:
+            print(f"{trackName[:trackName.find(word)]}{RED}red{RESET}{trackName[trackName.find(word)+len(word)-1]:}")
